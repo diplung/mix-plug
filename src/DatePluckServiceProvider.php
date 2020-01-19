3814c8b008d3533947caec83480b8f9fa7978d60
diplung <58248258+diplung@users.noreply.github.com>
2020-01-19 13:41:15 +0545

    Initial commit

---
+++ src/DatePluckServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace lung\MixPlug;
+
+use Illuminate\Support\ServiceProvider;
+
+class MixPLugServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        echo 'fsdfs';
+        die;
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+      
+    }
+}
---
+++ src/MixPlugController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lung\MixPlug;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MixPlugController extends Controller
+{
+    
+    public function render() {
+    	echo 'sdfsd';
+    }
+
+    public function is_user_data() {
+    }
+}
